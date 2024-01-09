5f9a3f3cc4b87d6270be7c333758e0f476fbdd00
webworld420 <76555387+webworld420@users.noreply.github.com>
2024-01-10 01:43:41 +0600

    homepage Faq Responsive done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 
+var faqs = document.querySelectorAll(".faq-item")
+
+faqs.forEach((item, i )=>{
+   item.addEventListener("click",()=>{
+      item.classList.toggle("active")
+   })
+})
 
 
-const header = document.getElementsByTagName(header);
 
-if (window.scrollY > 50) {
-    header.style="height:120px";
-}
