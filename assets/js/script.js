021f79ecf2463b9da9202dda56fcab0483a3b166
webworld420 <76555387+webworld420@users.noreply.github.com>
2024-01-11 01:38:19 +0600

    navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
 
+
+
+// NAVBAR MOBILE VERSION RESPONSIVE ICON
+
+   $("#bars-icon").click(function(){
+      alert("hello js")
+      $(".navigation-bar .nav-items").css("display", "none");;
+      $("#bars-icon span").toggle();
+   })
+
+// NAVBAR MOBILE VERSION RESPONSIVE ICON
+   // const navItems = document.querySelector(".navigation-bar .nav-items");
+   // const barsIcon = document.getElementById("bars-icon");
+
+   // barsIcon.addEventListener("click", ()=>{
+   //    navItems.style.display="block";
+   //    barsIcon.toggle("span");
+   // })
+
+
+
+
+
+// FAQ SECITON ACCORDIN TOGGLER
 var faqs = document.querySelectorAll(".faq-item")
 
 faqs.forEach((item, i )=>{
